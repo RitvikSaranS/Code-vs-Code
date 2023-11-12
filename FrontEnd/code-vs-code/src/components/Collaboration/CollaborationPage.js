7272c3c8af52131e7bba0d665a3f960e6db90986
RitvikSaranS <codebyritvik@gmail.com>
2023-11-12 15:44:47 +0530

    f-basiclayout

---
+++ FrontEnd/code-vs-code/src/components/Collaboration/CollaborationPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CollaborationPage = () => {
+  return (
+    <div>
+      <h1>Collaboration Page</h1>
+      <p>This is the collaboration page ladies and gentleman</p>
+    </div>
+  );
+}
+
+export default CollaborationPage;
