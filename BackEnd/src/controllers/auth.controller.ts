5aeb90acb41de7ceab0928d471e497abcb9904f3
RitvikSaranS <codebyritvik@gmail.com>
2024-01-20 19:23:55 +0530

    Implemented using Models

--- BackEnd/src/controllers/auth.controller.ts
+++ BackEnd/src/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { getUserByUsername, createUser } from "../services/user.service";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import logger from "../utils/logger";
+import { User } from "../models/user.model";
 require("dotenv").config();
 
 const secretKey: string | undefined = process.env.AUTH_SECRET_KEY;
@@ -13,16 +14,27 @@ export async function Register(req: Request, res: Response) {
   try {
     const { username, password, email, firstname, lastname, alternatemail } =
       req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await createUser(
-      username,
-      hashedPassword,
-      email,
-      firstname,
-      lastname,
-      alternatemail
-    );
-    res.status(201).json({ message: "User registered successfully" });
+    const userExists = await getUserByUsername(username);
+    if (userExists) {
+      res.status(409).send({ message: "User already exists, Please login" });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await createUser(
+        new User(
+          username,
+          hashedPassword,
+          email,
+          firstname,
+          lastname,
+          "",
+          "",
+          new Date(),
+          "",
+          "A"
+        )
+      );
+      res.status(201).json({ message: "User registered successfully" });
+    }
   } catch (error: any) {
     res.status(500).json({ error: "Registration failed" });
     logger(error.message, path.basename(__filename));
@@ -35,12 +47,16 @@ export async function Login(req: Request, res: Response) {
     const { username, password } = req.body;
     const user = await getUserByUsername(username);
     if (!user) {
-      return res.status(401).json({ error: "Authentication failed" });
+      return res
+        .status(401)
+        .json({ error: "Authentication failed!  Please register" });
     }
 
     const passwordMatch = await bcrypt.compare(password, user.UT_Password);
     if (!passwordMatch) {
-      return res.status(401).json({ error: "Authentication failed" });
+      return res
+        .status(401)
+        .json({ error: "Authentication failed! Incorrect Password" });
     }
 
     if (secretKey) {
